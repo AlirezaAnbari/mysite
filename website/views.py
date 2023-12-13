45c86bb567c87f327bba35247f722a178ae7e89e
Alireza Anbari <alireza.anbary@gmail.com>
2023-12-13 16:32:36 +0330

    create blog app

--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ def index_view(request):
     return render(request, 'website/index.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'website/contact.html')
