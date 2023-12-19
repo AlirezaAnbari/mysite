538a7d32486256299d56dd029aedff04f470f7bc
Alireza Anbari <alireza.anbary@gmail.com>
2023-12-19 21:43:59 +0330

    complete models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 
@@ -14,4 +14,5 @@ class PostAdmin(admin.ModelAdmin):
     # ordering = ['-created_date']
     search_fields = ['title', 'content']
     
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
