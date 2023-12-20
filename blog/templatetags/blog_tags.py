fd94a1835c00d0d9d052dd6a2d91b3a344c78a53
Alireza Anbari <alireza.anbary@gmail.com>
2023-12-20 20:14:29 +0330

    template tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from blog.models import Post
+
+
+register = template.Library()
+
+@register.simple_tag(name='totalposts') 
+def function():
+    posts = Post.objects.filter(status=1).count
+    return posts
+
+@register.simple_tag(name='posts') 
+def function():
+    posts = Post.objects.filter(status=1)
+    return posts     
+
+@register.filter
+def snippet(value, arg=20):
+    return value[:arg]
+
+@register.inclusion_tag('popularposts.html')
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('published_date')
+    return {'posts':posts}
