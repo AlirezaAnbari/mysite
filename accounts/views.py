b2181d04ed8cb3e776af550e7a5ce0855521ece7
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-09 19:38:49 +0330

    user authentication
    create a accounts app-
    get bootstrap for login page-
    create login from

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+def login_view(request):
+    if not request.user.is_authenticated():
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+            
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+        
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+
+    else:
+        return redirect('/')
+
+# def logout_view(request):
+#     return 
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'taggit',
     'website.apps.WebsiteConfig',
     'blog',
+    'accounts',
 ]
 
 SITE_ID = 1
