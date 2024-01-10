45e597b3c093bd60a0e97932f3a1e39e83b60366
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-10 18:38:41 +0330

    create signup form
    create signup form and change signin bootstrap(html ad css)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('signup', views.signup_view, name='signup'),
+    path('login/',views.login_view,name='login'),
+    # login
+    path('logout',views.logout_view,name='logout'),
+    # logout
+    path('signup/',views.signup_view,name='signup')
+    # registration / signup
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,35 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 def login_view(request):
-    if not request.user.is_authenticated :
+    if not request.user.is_authenticated:
         if request.method == 'POST':
-            form = AuthenticationForm(request=request, data=request.POST)
+            form = AuthenticationForm(request=request,data=request.POST)
             if form.is_valid():
                 username = form.cleaned_data.get('username')
                 password = form.cleaned_data.get('password')
                 user = authenticate(request, username=username, password=password)
-            
                 if user is not None:
-                    login(request, user)
+                    login(request,user)
                     return redirect('/')
-        
-        form = AuthenticationForm()
-        context = {'form': form}
-        return render(request, 'accounts/login.html', context)
 
+        form = AuthenticationForm()
+        context = {'form':form}
+        return render(request,'accounts/login.html',context)
     else:
         return redirect('/')
-    
+
 @login_required
 def logout_view(request):
-    
     logout(request)
-        
-    return redirect('/') 
+    return redirect('/')
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+                # return reverse('accounts:login') 
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request,'accounts/signup.html',context)
+    else:
+        return redirect('/')
